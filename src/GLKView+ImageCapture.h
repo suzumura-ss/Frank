135cb23e160e6f3adac0005be39c5616bc141e9b
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2013-06-06 00:23:41 +0900

    Impl. captureImage for GLKView.

---
+++ src/GLKView+ImageCapture.h
@@ -0,0 +1,15 @@
+//
+//  GLKView+ImageCapture.h
+//  Frank
+//
+//  Created by Toshiyuki Suzumura on 2013/06/05.
+//
+//
+
+#import <GLKit/GLKit.h>
+
+@interface GLKView (ImageCapture)
+
+- (UIImage *)captureImage;
+
+@end

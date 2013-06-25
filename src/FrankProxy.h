4a3e4fc4d841fbc02e1ac218a5d9987df89f3563
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2013-06-25 22:01:06 +0900

    Implement FrankProxy w/ARC.

---
+++ src/FrankProxy.h
@@ -0,0 +1,14 @@
+//
+//  FrankProxy.h
+//
+//  Created by Toshiyuki Suzumura on 2013/06/11.
+//  Copyright (c) 2013 Toshiyuki Suzumura All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrankProxy : NSObject
+
++ (void)run;
+
+@end
